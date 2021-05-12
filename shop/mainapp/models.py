c62b36f3a8015bcfeac6a64937d517ee1a28582b
Artur <geraskov94@mail.ru>
2021-05-12 13:31:13 +0200

    Add related name

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -39,7 +39,7 @@ class Product(models.Model):
 class CartProduct(models.Model):
 
     user = models.ForeignKey('Customer', verbose_name='Customer', on_delete=models.CASCADE)
-    cart = models.ForeignKey('Cart', verbose_name='Cart', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Cart', on_delete=models.CASCADE, related_name='related_product')
     product = models.ForeignKey(Product, verbose_name='Product', on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     sum_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Sum')
@@ -51,7 +51,7 @@ class CartProduct(models.Model):
 class Cart(models.Model):
 
     owner = models.ForeignKey('Customer', verbose_name='Owner', on_delete=models.CASCADE)
-    product = models.ManyToManyField(CartProduct, blank=True)
+    product = models.ManyToManyField(CartProduct, blank=True,  related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
     sum_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Sum')
 
