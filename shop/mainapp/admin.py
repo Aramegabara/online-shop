5b33e0aecb14ed8e0cd188a43f3c493242712cea
Artur <geraskov94@mail.ru>
2021-05-12 18:29:09 +0200

    Min and max resolution for image (+style)

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,23 +1,29 @@
+from django.forms import ModelChoiceField, ModelForm
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from xdg.Exceptions import ValidationError
 from .models import *
-from django.forms import ModelChoiceField, ModelForm
 from PIL import Image
 
 class NoebookAdminForm(ModelForm):
 
     MIN_RESOLUTION = (400, 400)
-    # MAX_RESOLUTION = ()
+    MAX_RESOLUTION = (800, 800)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = 'Download image (min {}x{})'.format(*self.MIN_RESOLUTION)
+        self.fields['image'].help_text = mark_safe(
+            '''<span style="color:red; font-size:14px;">
+                    Load image (min {}x{}, max{}x{})
+               </span>'''.format(*self.MIN_RESOLUTION, *self.MAX_RESOLUTION))
 
     def clean_image(self):
         image = self.clean_data['image']
         img = Image.open(image)
-        # print(img.width, img.height)
         min_height, min_width = self.MIN_RESOLUTION
+        max_height, max_width = self.MAX_RESOLUTION
+        if img.width > max_width or img.height > max_height:
+            raise ValidationError('Big image!')
         if img.width < min_width or img.height < min_height:
             raise ValidationError('Small image!')
         return image
