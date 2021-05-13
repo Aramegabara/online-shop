d97bfafab47ef102c41d4afa6f84e58629aa737c
Artur <geraskov94@mail.ru>
2021-05-13 21:55:06 +0200

    (fix) show product info

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -16,14 +16,11 @@ class ProductDetailView(DetailView):
     }
 
     def dispatch(self, request, *args, **kwargs):
-        # ct_model = kwargs.get('ct_model') #CT_MODEL_MODEL_CLASS
         self.model = self.CT_MODEL_MODEL_CLASS[kwargs['ct_model']]
         self.queryset = self.model._base_manager.all()
         return super().dispatch(request, *args, **kwargs)
 
 
-    # model = Model
-    # queryset = Model.objects.all()
-    content_object_name = 'product'
+    context_object_name = 'product'
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
