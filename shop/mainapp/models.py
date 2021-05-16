294b3a178f0e9843a8bb8f8f19e63c5303c7c045
Artur <geraskov94@mail.ru>
2021-05-16 23:05:19 +0200

    Chek

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -165,7 +165,7 @@ class CartProduct(models.Model):
 class Cart(models.Model):
 
     owner = models.ForeignKey('Customer', verbose_name='Owner', on_delete=models.CASCADE)
-    product = models.ManyToManyField(CartProduct, blank=True,  related_name='related_cart')
+    product = models.MzanyToManyField(CartProduct, blank=True,  related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
     sum_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Sum')
     in_order = models.BooleanField(default=False)
