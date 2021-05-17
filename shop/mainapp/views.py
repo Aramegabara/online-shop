619088ba3326cfa0e17505e956d53d19d64a30f5
Artur <geraskov94@mail.ru>
2021-05-17 17:53:20 +0200

    Add mixins

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
 from .models import Notebook, Smartphone, Category
+from .mixins import CategoryDetailMixin
+
+class BaseView(View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        return render(request, 'mainapp/base.html', {'categories': categories})
 
 
-def test_view(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'mainapp/base.html', {'categories': categories})
-
-
-class ProductDetailView(DetailView):
+class ProductDetailView(CategoryDetailMixin, DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
@@ -28,7 +30,7 @@ class ProductDetailView(DetailView):
 
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
 
     model = Category
     queryset = Category.objects.all()
