1909d82e2eae591ff5b9ba9feffeaacb313cd731
Artur <geraskov94@mail.ru>
2021-05-18 17:45:49 +0200

    Cart register and check owner

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -175,10 +175,10 @@ class CartProduct(models.Model):
 
 class Cart(models.Model):
 
-    owner = models.ForeignKey('Customer', verbose_name='Owner', on_delete=models.CASCADE)
+    owner = models.ForeignKey('Customer', null=True, verbose_name='Owner', on_delete=models.CASCADE)
     product = models.ManyToManyField(CartProduct, blank=True,  related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
-    sum_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Sum')
+    sum_price = models.DecimalField(max_digits=9, default=0, decimal_places=2, verbose_name='Sum')
     in_order = models.BooleanField(default=False)
     for_anonymous_user = models.BooleanField(default=False)
 
@@ -190,8 +190,8 @@ class Cart(models.Model):
 class Customer(models.Model):
 
     user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20, verbose_name='Phone')
-    address = models.CharField(max_length=255, verbose_name='Address')
+    phone = models.CharField(max_length=20, verbose_name='Phone', null=True)
+    address = models.CharField(max_length=255, verbose_name='Address', null=True)
 
     def __str__(self):
         return f"Customer : {self.user.first_name} {self.user.last_name}"
