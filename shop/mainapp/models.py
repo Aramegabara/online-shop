da2f359f50a43857693d0b1f6828bef4910d6e0d
Artur <geraskov94@mail.ru>
2021-05-18 17:51:14 +0200

    Fix

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -190,8 +190,8 @@ class Cart(models.Model):
 class Customer(models.Model):
 
     user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20, verbose_name='Phone', null=True)
-    address = models.CharField(max_length=255, verbose_name='Address', null=True)
+    phone = models.CharField(max_length=20, verbose_name='Phone', null=True, blank=True)
+    address = models.CharField(max_length=255, verbose_name='Address', null=True, blank=True)
 
     def __str__(self):
         return f"Customer : {self.user.first_name} {self.user.last_name}"
