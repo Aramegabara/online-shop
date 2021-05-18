d9ee2e884692a31cc4cee29c364914b7822b06ed
Artur <geraskov94@mail.ru>
2021-05-18 14:36:41 +0200

    Add product to cart

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
-from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
 from .mixins import CategoryDetailMixin
+
 
 class BaseView(View):
 
@@ -30,11 +33,14 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
         self.queryset = self.model._base_manager.all()
         return super().dispatch(request, *args, **kwargs)
 
-
     context_object_name = 'product'
     template_name = 'mainapp/product_detail.html'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
 
 
 class CategoryDetailView(CategoryDetailMixin, DetailView):
@@ -44,6 +50,23 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     context_object_name = 'category'
     template_name = 'mainapp/category_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class AddToCartView(View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer, in_order=False)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product = CartProduct.objects.create(
+            user=cart.owner, cart=cart, content_object=product, sum_price=product.price
+        )
+        cart.product.add(cart_product)
+
+        return  HttpResponseRedirect('/cart/')
+
 
 class CartView(View):
 
