139cde1036b23f3d6564acd7bf80757fd0f6891b
Artur <geraskov94@mail.ru>
2021-05-19 22:26:04 +0200

    Make animation for shop name

--- shop/static_dev/js/milk.js
+++ shop/static_dev/js/milk.js
@@ -1,1 +1,31 @@
-alert('Hello');
+const text  = document.querySelector(".shop_name");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = " ";
+for (let i = 0; i < splitText.length; i++){
+    text.innerHTML += "<h6>" + splitText[i] + "</h6>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 90);
+
+function onTick() {
+    const h6 = text.querySelectorAll("h6")[char];
+    h6.classList.add("fade");
+    char++;
+    if (char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+    text.textContent = strText;
+
+}
+
+const nav = document.querySelectorAll(".list-group-item");
+
+
